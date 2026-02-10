5655ab68689d37bd4b9a793a6ef2e37453544592
Yanik Thurner <yanik@thurner.dev>
2026-02-10 17:37:15 +0100

    moved lua script to tmx folder, reimplemented terminal appreach cause baleia slow af on large buffers

--- .config/tmux/nvim-copy-mode.sh
+++ .config/tmux/nvim-copy-mode.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 # ~/.config/tmux/scripts/nvim-copy-mode.sh
 
-file=$(mktemp --suffix=.tmux-copy)
+read -r pane_id cursor_x cursor_y history_size pane_width pane_height <<< "$(tmux display -p '#{pane_id} #{cursor_x} #{cursor_y} #{history_size} #{pane_width} #{pane_height}')"
 
-read -r cy hs cx pw ph <<< "$(tmux display -p '#{cursor_y} #{history_size} #{cursor_x} #{pane_width} #{pane_height}')"
-line=$((hs + cy + 1))
+filename="/dev/shm/tmux-cap-$$"
 
-tmux capture-pane -e -p -N -S - -E - > "$file"
+tmux capture-pane -t "$pane_id" -p -e -J -S - -E - > "$filename"
 
-tmux popup -s 'default' -E -B -x P -y P -w "$pw" -h "$ph" \
-  env COPY_LINE="$line" COPY_COL="$cx" \
-  nvim --clean --noplugin -u ~/.config/nvim/copy-mode.lua --cmd 'set lazyredraw' -R "$file"
+echo "pane_id:$pane_id cursor_x:$cursor_x cursor_y:$cursor_y history_size:$history_size pane_width:$pane_width pane_height:$pane_height" > /tmp/copy-mode-debug.txt
 
-rm -f "$file"
+exec tmux display-popup -xP -yP -w "$pane_width" -h "$pane_height" -E -B \
+  env CURSOR_Y="$(( $history_size + $cursor_y + 1 ))" CURSOR_X="$cursor_x" COPY_FILE="$filename" \
+  nvim --clean -u "~/.config/tmux/copy-mode.lua"
