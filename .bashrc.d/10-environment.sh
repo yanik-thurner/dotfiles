e9eef3934994a87dbab016623e5a6aee16bd55d8
Yanik Thurner <yanik@thurner.dev>
2026-02-11 17:41:21 +0100

    adds cargo bin folder to PATH

--- .bashrc.d/10-environment.sh
+++ .bashrc.d/10-environment.sh
@@ -6,3 +6,5 @@ export XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
 export XDG_STATE_HOME="${XDG_STATE_HOME:-$HOME/.local/state}"
 export XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
 export XDG_RUNTIME_DIR="${XDG_RUNTIME_DIR:-/run/user/$(id -u)}"
+
+export PATH="$PATH:$HOME/.cargo/bin:/aoeuaeou"
