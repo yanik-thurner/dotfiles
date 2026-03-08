b498323e567598d85eb0b54a8fc1d49e07026634
Yanik Thurner <yanik@thurner.dev>
2026-03-08 14:07:12 +0100

    enabled vi bindings in fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,7 @@ alias bash='env NO_FISH=1 bash'
 if status is-interactive
     # Commands to run in interactive sessions can go here
     source ~/.bashrc.d/*-aliases.sh
+    fish_vi_key_bindings
 end
 
 # Added by LM Studio CLI (lms)
